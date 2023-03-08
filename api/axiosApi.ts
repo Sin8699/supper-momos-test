d0a1e90d8d6c2d19ec00b40db85e12761b9530cb
Phuong <phuong.dao@inkr.com>
2023-03-09 04:44:44 +0700

    try to fix

--- api/axiosApi.ts
+++ api/axiosApi.ts
@@ -4,5 +4,9 @@ export const axiosInstance = axios.create({
   baseURL: "https://api.supermomos-dev.com",
   timeout: 1000,
   withCredentials: false,
-  headers: { "Content-Type": "application/json" },
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+  },
 });
