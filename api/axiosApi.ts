1d00a4f187260e2074df8ff6f5aec93298b0afc7
Phuong <phuong.dao@inkr.com>
2023-03-09 04:37:19 +0700

    fix cor

--- api/axiosApi.ts
+++ api/axiosApi.ts
@@ -3,5 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: "https://api.supermomos-dev.com",
   timeout: 1000,
+  withCredentials: false,
   headers: { "Content-Type": "application/json" },
 });
